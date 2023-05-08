2bbd33805556c446799e2523cab9cf8346cf8ca4
Joel Johny <76478362+Joel-Johny@users.noreply.github.com>
2023-05-08 21:30:20 +0530

    performed simple validation and added svg on error

--- script.js
+++ script.js
@@ -1,5 +1,34 @@
+let valid =true
 
+function error(className,message){
+    let elements=Array.from(document.getElementsByClassName(className))
+    elements.forEach((element)=>{
+    element.style.display='block'
+    })
+    elements[1].textContent=message
+    valid=false
+}
 function validate(){
-    console.log("asdasdasd")
 
+   let form=document.forms["details"]
+   
+   let fname=form.fname.value
+   let lname=form.lname.value
+   let email=form.email.value
+   let pass=form.password.value
+
+   if(fname.length==0)
+        error("fname-message","First Name cannot be empty")
+    
+   if(lname.length==0)
+        error("lname-message","Last Name cannot be empty")
+
+    if(!(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).test(email))
+        error("email-message","Looks like this is not an email")
+
+    if(pass.length==0)
+        error("pass-message","Password cannot be empty")
+
+
+    return valid
 }
